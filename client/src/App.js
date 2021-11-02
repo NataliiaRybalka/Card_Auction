2e45600b6626fac679e8c287985b222ad4271bed
NataliiaRybalka <natalirybalka21@gmail.com>
2021-11-02 18:04:02 +0200

    install redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
-import { useState } from 'react';
-import io from 'socket.io-client';
-
 import './App.css';
-import { LOCALHOST } from "./constants/contants";
-
-const socket = io(LOCALHOST, { transports : ['websocket'] });
 
 function App() {
-  const [auction, setAuction] = useState();
-
-  socket.on('update auction rate', (auction) => {
-    setAuction(auction);
-  });
-  console.log(auction);
 
   return (
     <div className="App">
