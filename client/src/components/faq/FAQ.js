88d2a9a106ded2a686d378f29a47338df081fe69
NataliiaRybalka <natalirybalka21@gmail.com>
2021-11-22 14:53:26 +0200

    faq done

--- client/src/components/faq/FAQ.js
+++ client/src/components/faq/FAQ.js
@@ -21,23 +21,3 @@ export const FAQ = () => {
     </div>
   );
 };
-
-{/* 
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
-import { Cards } from '../cards/Cards';
-import { Sets } from '../sets/Sets';
-
-<Router>
-  <div className={'main faqPage'}>
-    <Link to='/faq/cards' className={'navLinks'}> <h2 className={'faqBtn faqBtnCards'}>Cards</h2> </Link>
-    <Link to='/faq/sets' className={'navLinks'}> <h2 className={'faqBtn'}>Sets</h2> </Link>
-
-    <Redirect to='/faq/cards' />
-  </div>
-
-  <Switch>
-    <Route path='/faq/cards' component={Cards} />
-    <Route path='/faq/sets' component={Sets} />
-  </Switch>
-</Router>
-*/}
