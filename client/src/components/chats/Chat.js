9510958d17a9512c876945b6643c92524dd099c1
NataliiaRybalka <natalirybalka21@gmail.com>
2021-11-30 17:29:02 +0200

    chat with db

--- client/src/components/chats/Chat.js
+++ client/src/components/chats/Chat.js
@@ -1,22 +1,24 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { v1 } from 'uuid';
 
 import { getChat, sendMessage } from "../../redux/actions/chats.actions";
 import { socket } from "../../constants/socket";
 
 export const Chat = () => {
   const [message, setMessage] = useState('');
-  const [messageList, setMessageList] = useState([]);
   const dispatch = useDispatch();
   const chat = useSelector(state => state.chatsReducer.chat);
   const room = localStorage.getItem('room');
+  const [messageList, setMessageList] = useState([]);
 
   useEffect(() => {
     dispatch(getChat(localStorage.getItem('toUserId')));
   }, [dispatch]);
 
   useEffect(() => {
-      window.scrollTo(0,document.body.scrollHeight);
+    setMessageList(chat);
+    window.scrollTo(0,document.body.scrollHeight);
   }, [chat]);
 
   const onChangeInputHandler = e => {
@@ -33,6 +35,10 @@ export const Chat = () => {
         time: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes()
       };
 
+      dispatch(sendMessage({
+        chatId: chat[0].chat_id,
+        messageData
+      }))
       await socket.emit('send_message', messageData);
       setMessageList((list) => [...list, messageData]);
 
@@ -44,7 +50,7 @@ export const Chat = () => {
     socket.on('receive_message', (data) => {
       setMessageList((list) => [...list, data]);
     });
-  }, []);
+  }, [socket]);
 
   return (
     <div className={'main'}>
@@ -54,7 +60,7 @@ export const Chat = () => {
 
       <ul id={'chatList'}>
         {!!messageList.length && messageList.map(msgData => (
-          <li key={msgData.room + msgData.message} className={(msgData.from === localStorage.getItem('id')) ? 'chatMessage fromMessage' : 'chatMessage toMessage'}>
+          <li key={msgData.message + v1()} className={(+msgData.from === +localStorage.getItem('id')) ? 'chatMessage fromMessage' : 'chatMessage toMessage'}>
             <span className={'chatMessageText'}>{msgData.message}</span>
             <br />
             <span className={'chatMessageTime'}>{msgData.time}</span>
